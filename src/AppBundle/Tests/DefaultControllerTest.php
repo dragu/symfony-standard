ccf2d855b1b4614c07a980b7b1cdd5b04a4a9914
dragu <szymon@demotywatory.pl>
2016-08-30 09:37:21 +0200

    tests stuff

---
+++ src/AppBundle/Tests/DefaultControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testCsrfRequired()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/csrf');
+        $response = $client->getResponse();
+
+		$this->assertSame('The CSRF token is invalid. Please try to resubmit the form.', $response->getContent());
+    }
+
+    public function testCsrfIsDisabledByExtension()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/no-csrf');
+        $response = $client->getResponse();
+
+		$this->assertSame('OK', $response->getContent());
+    }
+}
